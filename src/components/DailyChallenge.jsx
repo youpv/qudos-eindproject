3fd97b20dfc7112ffa9d913f3d8203da04c82b2e
Youp V <youpverkooijen@gmail.com>
2022-11-25 12:46:53 +0100

    Added daily challenge system based on users

--- src/components/DailyChallenge.jsx
+++ src/components/DailyChallenge.jsx
@@ -1,21 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Triangle from '../img/triangle-btn-blue.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import tasks from '../data/NameTasks.json'
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+
 
 const DailyChallenge = () => {
   const [show, toggleShow] = useState(false);
+  const [task, setTask] = useState("");
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUserNames() {
+      // fetch all displayNames from every user in users
+      await getDocs(collection(db, "users"))
+        .then((querySnapshot) => {
+          const list = querySnapshot.docs.map((doc) => doc.data().displayName);
+          setUsers(list);
+        })
+        .catch((error) => {
+          console.log("Error getting documents: ", error);
+        })
+    }
+    getUserNames();
+  }, []);
+
+
 
   const onCompleted = (state) => {
     toggleShow(!show);
-
-    // hier logica om de challenge te verwijderen uit de database wanneer de challenge is voltooid
   }
-  
+
+  const handleClick = () => {
+    toggleShow(true);
+    let randomUser = users[Math.floor(Math.random() * users.length)];
+    let randomTaskNamed = tasks[1].NameNeeded[Math.floor(Math.random() * tasks[1].NameNeeded.length)];
+    let randomTaskUnnamed = tasks[0].noNameNeeded[Math.floor(Math.random() * tasks[0].noNameNeeded.length)];
+    let randomTask = Math.random() < 0.5 ? randomTaskNamed + " " + randomUser : randomTaskUnnamed;
+    setTask(randomTask);
+  }
+
 
   return (
     <>
-      <div className="container daily-challenge" onClick={() => toggleShow(true)}>
+      <div className="container daily-challenge" onClick={handleClick}>
         <div className="row">
           <div className="col-sm-12 daily-challenge-holder">
             <div className="daily-challenge shadow-block blue-shadow">
@@ -27,21 +57,21 @@ const DailyChallenge = () => {
       </div>
 
       {show && (
-        
-            <div className="col-sm-12 dc-popup-holder">
-              <div className="dc-popup col-sm-8 shadow-block blue-shadow">
-                <h3>Daily challenge</h3>
 
-                <div id="challengeText">Hallo</div>
-                <div className="dc-btns">
-                  <button className="btn btn-afgerond" onClick={() => onCompleted(true)}>Afgerond</button>
-                  <button className="btn sec-btn" onClick={() => onCompleted(false)}>Annuleren</button>
-                </div>
+        <div className="col-sm-12 dc-popup-holder">
+          <div className="dc-popup col-sm-8 shadow-block blue-shadow">
+            <h3>Daily challenge</h3>
 
-                <FontAwesomeIcon icon={faXmark} className="close-btn-dc-popup" onClick={() => toggleShow(false)}/>
-
-              </div>
+            <div id="challengeText">{task}</div>
+            <div className="dc-btns">
+              <button className="btn btn-afgerond" onClick={() => onCompleted(true)}>Afgerond</button>
+              <button className="btn sec-btn" onClick={() => onCompleted(false)}>Annuleren</button>
             </div>
+
+            <FontAwesomeIcon icon={faXmark} className="close-btn-dc-popup" onClick={() => toggleShow(false)} />
+
+          </div>
+        </div>
 
       )}
     </>
