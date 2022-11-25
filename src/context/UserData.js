f4de1555335f28635a7e870497ce547dc94c186e
Youp V <youpverkooijen@gmail.com>
2022-11-25 12:19:41 +0100

    userdata is no longer used

--- src/context/UserData.js
+++ src/context/UserData.js
@@ -1,25 +1,25 @@
-import { useState, useEffect, useContext } from "react";
-import { AuthContext } from "./AuthContext";
-import { doc, onSnapshot } from "firebase/firestore";
-import { db } from "../firebase";
+// import { useState, useEffect, useContext } from "react";
+// import { AuthContext } from "./AuthContext";
+// import { doc, onSnapshot } from "firebase/firestore";
+// import { db } from "../firebase";
 
-export default function UserData() {
-	const { currentUser } = useContext(AuthContext);
-	const [userData, setUserData] = useState();
-	useEffect(() => {
-		if (currentUser) {
-			try {
-				const unsubscribe = onSnapshot(
-					doc(db, "users", currentUser.uid),
-					(doc) => {
-						setUserData(doc.data());
-					}
-				);
-				return unsubscribe;
-			} catch (error) {
-				console.log(error);
-			}
-		}
-	}, [currentUser]);
-	return userData || {"firstName": "", "mood": "🔁"};
-}
+// export default function UserData() {
+// 	const { currentUser } = useContext(AuthContext);
+// 	const [userData, setUserData] = useState();
+// 	useEffect(() => {
+// 		if (currentUser) {
+// 			try {
+// 				const unsubscribe = onSnapshot(
+// 					doc(db, "users", currentUser.uid),
+// 					(doc) => {
+// 						setUserData(doc.data());
+// 					}
+// 				);
+// 				return unsubscribe;
+// 			} catch (error) {
+// 				console.log(error);
+// 			}
+// 		}
+// 	}, [currentUser]);
+// 	return userData || {"firstName": "", "mood": "🔁"};
+// }
